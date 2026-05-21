9605b737a8b5cb7632d4d1032b530d1e63677410
Fahd Chafai <fahdchafai427@gmail.com>
2026-05-21 13:13:27 +0100

    chore(student-service): scaffold Maven Spring Boot project

---
+++ student-enrollment-system/student-service/src/test/java/com/enrollment/student/StudentServiceApplicationTests.java
@@ -0,0 +1,10 @@
+package com.enrollment.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentServiceApplicationTests {
+    @Test
+    void contextLoads() {}
+}
